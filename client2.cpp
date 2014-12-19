3cefae728a39dd0d864227942d912f5670ca3515
xuyang <xuyang.0801@qq.com>
2014-12-19 17:04:52 +0800

    删除掉一些注释

--- client2.cpp
+++ client2.cpp
@@ -3,17 +3,10 @@
 #include <iostream>
 #include <objbase.h>
 #include "iface.h"
-//#include "create.h"
 using namespace std;
 int main()
 {
-    //MyCal* pMyCal = new MyCal;
     HRESULT hr;
-    //  IUnknown* pI = CallCreateInstance("CMTNT1.dll");
-    //if(pI==NULL)
-    //{
-    //  return -1;
-    //}
     hr = CoInitialize ( NULL );
     
     if ( FAILED ( hr ) )
@@ -25,13 +18,13 @@ int main()
     int array[10] = {3, 34, 45, 22, 44, 432, 22, 75, 43, 21 };
     ICalBase* pICalBase = NULL;
     hr =::CoCreateInstance ( CLSID_COMP1, NULL, CLSCTX_INPROC_SERVER, IID_CALBASE, ( LPVOID* ) &pICalBase );
-    // hr=pI->QueryInterface(IID_CALBASE, (void**)&pICalBase);
+    
     
     if ( SUCCEEDED ( hr ) )
     {
         res = pICalBase->Add ( 1, 2 );
         res = pICalBase->Mul ( 33, 44 );
-        //pICalBase->Release();
+       
     }
     
     ICalStd* PICalStd = NULL;
